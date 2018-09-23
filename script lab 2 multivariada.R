9fda38d07da94e5364b4ecfa78b283dc3313c580
KevinChica1128 <kevin.chica@correounivalle.edu.co>
2018-09-23 08:29:06 -0500

    .

--- script lab 2 multivariada.R
+++ script lab 2 multivariada.R
@@ -2,3 +2,33 @@
 # Alejandro Vargas - 1525953
 # Alejandro Soto - 1532457
 # Laboratorio 2 - Multivariada
+
+#base de datos
+provincias=read.table("clipboard",header=T)
+View(provincias)
+#*****Lab 2
+#funciones
+#Porcentaje de incercia
+library("ade4")
+library("FactoMineR")
+x11()
+PCA.results=PCA(provincias)
+PCA.results$eig[2,3]#porcentaje de varianza explicada por los dos primeros ejes
+X11()
+barplot(PCA.results$eig[,2])#grafico del porcentaje explicado
+#1)nube de individuos
+X11()
+plot(PCA.results)#grafico de los individuos dos primeras dimenciones
+#2)Circulo de correlaciones
+plotellipses(PCA.results)
+#3) representacion simultanea
+library(factoextra)
+x11()
+fviz_pca_biplot(PCA.results)
+#4)contribuciones y concenos cuadrados
+PCA.results$var$cos2#cocenos cuadrados
+PCA.results$var$contrib#contribuciones
+#f) Descriptivas
+summary(provincias)
+X11()
+boxplot(provincias)
